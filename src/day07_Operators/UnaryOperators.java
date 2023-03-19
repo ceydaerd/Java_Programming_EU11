f557cfc8e0519d24c55c20ff04dd478a229a2e36
Ceyda ERDOGDU <erdogdufece@gmail.com>
2023-03-19 16:12:39 +0300

    Day5 one more time

--- src/day07_Operators/UnaryOperators.java
+++ src/day07_Operators/UnaryOperators.java
@@ -22,5 +22,42 @@ public class UnaryOperators {
 
         System.out.println(a);
 
+        System.out.println("-------------------------");
+
+        int b = 100;
+
+        System.out.println(++b); //pre increment: increases the value by 1 right away
+
+        int c = 100;
+
+        System.out.println(c++); //post increment:first passes the current value,then increases the value by1
+        System.out.println(c); //101
+
+        int x = 200;
+
+        System.out.println( --x ); //pre decrement:
+
+        int y = 200;
+
+        System.out.println( y-- ); //post decrement : first passes the current value, then decreases the value by1
+        System.out.println(y);
+
+        System.out.println("---------------------------");
+
+
+        int z= 45;
+        System.out.println( ++z);
+        System.out.println( z++);
+        System.out.println(z);
+
+
+        int q =30;
+
+        System.out.println(--q);
+        System.out.println(q--);
+        System.out.println(q);
+
+
+
     }
 }
